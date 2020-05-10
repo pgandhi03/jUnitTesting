adba4bca5b3219fc21b7d08689e99578767c84fc
Prachi Gandhi <itsprachi03@gmail.com>
2020-05-10 16:40:53 +0530

    Updating

--- JUnitTestExample/src/jUnitTesting.java
+++ JUnitTestExample/src/jUnitTesting.java
@@ -4,6 +4,10 @@ public class jUnitTesting {
 	int addNumbers(int a,int b) {
 		return a+b;
 	}
+	
+	String addString(String a,String b) {
+		return a+b;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
