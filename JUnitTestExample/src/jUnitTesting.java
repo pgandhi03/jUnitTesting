16c0a4cd3a9ea8133c6b81e2214620cac793dac0
Prachi Gandhi <itsprachi03@gmail.com>
2020-05-10 15:24:38 +0530

    Upload

---
+++ JUnitTestExample/src/jUnitTesting.java
@@ -0,0 +1,14 @@
+
+public class jUnitTesting {
+	
+	int addNumbers(int a,int b) {
+		int c=a+b;
+		return c;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
