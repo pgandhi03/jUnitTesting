92a0815c416a581dc8d4ec00d4542479986f369c
Prachi Gandhi <itsprachi03@gmail.com>
2020-05-10 16:43:23 +0530

    Uploading test for add strings

---
+++ JUnitTestExample/src/testAddStrings.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testAddStrings {
+
+	@Test
+	void test() {
+		jUnitTesting junit=new jUnitTesting();
+		String result=junit.addString("hello", "world");
+		assertEquals("helloworld",result);
+	}
+
+}
